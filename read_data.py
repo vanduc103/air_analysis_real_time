f0fe5b7b29bde500edb496c15590602092b6344d
Le Van Duc <duclv@kdb.snu.ac.kr>
2018-04-05 14:34:25 +0900

    update read_data.py

--- read_data.py
+++ read_data.py
@@ -1,6 +1,5 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.colors as c
 from scipy.misc import imresize
 import time
 
@@ -8,7 +7,7 @@ N = 1024
 i=-1
 X = list()
 # read pm2_5 values
-with open('data_01_2018.csv', 'r') as f:
+with open('data/data_01_2018.csv', 'r') as f:
     next(f)
     for row in f:
         res = row.split(',')
@@ -26,9 +25,9 @@ with open('data_01_2018.csv', 'r') as f:
 
 # read AQI label
 y = list()
-with open('aqi_01_2018', 'r') as f:
+with open('data/aqi_01_2018', 'r') as f:
     for row in f:
-        y.append(int(row))
+        y.append(float(row))
 
 # show values
 m = timerange + 1
@@ -39,15 +38,15 @@ y = y[0:m]
 print(y.shape)
 y_ = np.copy(y)
 y_[np.argwhere(y <= 15)[:,0]] = 0
-y_[np.argwhere(np.logical_and(y > 15, y <= 35))[:,0]] = 1
-y_[np.argwhere(np.logical_and(y > 35, y <= 75))[:,0]] = 2
-y_[np.argwhere(y > 75)[:,0]] = 3
+y_[np.argwhere(np.logical_and(y > 15, y <= 50))[:,0]] = 1
+y_[np.argwhere(np.logical_and(y > 50, y <= 100))[:,0]] = 2
+y_[np.argwhere(y > 100)[:,0]] = 3
 #print(np.count_nonzero(y_ == 0))
 #print(np.count_nonzero(y_ == 1))
 #print(np.nonzero(y_ == 2))
 #print(np.count_nonzero(y_ == 3))
 
-#Visualizing CIFAR 10
+#Visualizing as image
 X = X.reshape(m, 32, 32)
 X0 = X[4]
 points = list()
@@ -62,28 +61,20 @@ values = np.asarray(values).reshape(-1,1)
 print(points.shape)
 print(values.shape)
 
-from sklearn.neighbors import KNeighborsRegressor
-neigh = KNeighborsRegressor(n_neighbors=3, weights='distance')
-neigh.fit(points, values)
-print(neigh.predict([28,25])[0][0])
-
-'''norm = c.Normalize()
-for i in range(40, 69):
+for i in range(45, 50):
     fig = plt.figure()
     #i = np.random.choice(range(len(X)))
     ax = fig.add_subplot(121)
-    ax.set_title('i = ' + str(i) + ', y = ' + str(y_[i]))
+    ax.set_title('i = ' + str(i) + ', y = ' + str(int(y_[i])))
     X_i = X[i:i+1][0]
-    #X_i = imresize(X_i, (227, 227))
-    ax.imshow(X_i, cmap='gray', norm=norm, interpolation='bilinear')
+    ax.imshow(X_i, cmap='gray', interpolation='bilinear')
 
     #i = np.random.choice(range(len(X)))
     ax = fig.add_subplot(122)
-    ax.set_title('i = ' + str(i+1) + ', y = ' + str(y_[i+1]))
+    ax.set_title('i = ' + str(i+1) + ', y = ' + str(int(y_[i+1])))
     X_i1 = X[i+1:i+2][0]
-    #X_i1 = imresize(X_i1, (227, 227))
-    ax.imshow(X_i1, cmap='gray', norm=norm, interpolation='bilinear')
+    ax.imshow(X_i1, cmap='gray', interpolation='bilinear')
 
-    plt.show()'''
+    plt.show()
 
 
